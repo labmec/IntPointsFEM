0d7cb127d6e391f2d86de7a3312aaca0bb7f5648
Omar Duran <omaryesiduran@gmail.com>
2019-04-19 20:01:39 -0300

    Small change

--- main.cpp
+++ main.cpp
@@ -93,7 +93,7 @@ int main(int argc, char *argv[]) {
     
 // Calculates the solution using Newton method
     int n_iterations = 50;
-    REAL tolerance = 1.e-5; /// NVB this tolerance is more apropriated for nonlinear problems
+    REAL tolerance = 1.e-5;
    Solution(analysis, n_iterations, tolerance);
 
 // Post process
