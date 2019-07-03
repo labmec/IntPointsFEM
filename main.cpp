0c6c0812d1127185366c6ca059b5c93e5cb36963
Omar Duran <omaryesiduran@gmail.com>
2019-07-03 11:34:44 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -100,7 +100,7 @@ int main(int argc, char *argv[]) {
     
 // Calculates the solution using Newton method
     int n_iterations = 80;
-    REAL tolerance = 1.e-3;
+    REAL tolerance = 1.e-4;
     // {
         timer.Start();
         Solution(analysis, n_iterations, tolerance);
