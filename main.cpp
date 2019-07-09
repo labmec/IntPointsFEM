8ccb54ad5505dc24dcb87025134e98714c5024b7
Omar Duran <omaryesiduran@gmail.com>
2019-07-08 22:31:43 -0300

    ....

--- main.cpp
+++ main.cpp
@@ -64,8 +64,10 @@ int main(int argc, char *argv[]) {
     std::string source_dir = SOURCE_DIR;
    // std::string msh_file = source_dir + "/gmsh/wellbore_15p876k.msh";
    // std::string msh_file = source_dir + "/gmsh/wellbore_64p516k.msh";
-   // std::string msh_file = source_dir + "/gmsh/wellbore_260p100k.msh";
-    std::string msh_file = source_dir + "/gmsh/wellbore-coarse.msh";
+//    std::string msh_file = source_dir + "/gmsh/wellbore_260p100k.msh";
+    std::string msh_file = source_dir + "/gmsh/wellbore_1044p484k,msh";
+//    std::string msh_file = source_dir + "/gmsh/wellbore-coarse.msh";
+//    std::string msh_file = source_dir + "/gmsh/wellbore.msh";
     TPZGeoMesh *gmesh = ReadGeometry(msh_file);
 #ifdef PZDEBUG
     PrintGeometry(gmesh);
