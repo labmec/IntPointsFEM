16d71bae5588f50d0c4d9eb5bfd141570043a0e2
Omar Duran <omaryesiduran@gmail.com>
2019-07-19 14:08:42 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -253,7 +253,7 @@ void Solution(TPZAnalysis *analysis, int n_iterations, REAL tolerance, bool modi
         std::cout << "Calling AssembleResidual: Elasped time [sec] = " << timer.ElapsedTime() << std::endl;
 
         norm_res = Norm(analysis->Rhs());
-        stop_criterion_Q = norm_res < tolerance & norm_delta_du < tolerance;
+        stop_criterion_Q = norm_res < tolerance /*& norm_delta_du < tolerance*/;
         std::cout << "Nonlinear process : delta_du norm = " << norm_delta_du << std::endl;
         std::cout << "Nonlinear process : residue norm = " << norm_res << std::endl;
         if (stop_criterion_Q) {
